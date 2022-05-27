4d26dc89761b72af1263649c33a1acc18f4eadcb
Thomas Spencer <thomwilliamspencer2@gmail.com>
2022-05-27 14:36:04 +1000

    test

--- Item 4/MapItem.cs
+++ Item 4/MapItem.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 
 namespace Item_4
 {
-    internal interface MapItem
+    internal abstract class MapItem
     {
+        private int xPos;
+        private int yPos;
     }
 }
