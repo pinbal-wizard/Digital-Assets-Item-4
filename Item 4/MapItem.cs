b9ab72cc4ad89bb3b93ea8c4aebaa21c08b36648
Thomas Spencer <thomwilliamspencer2@gmail.com>
2022-05-31 12:26:34 +1000

    Added UML diagram (might not work)

--- Item 4/MapItem.cs
+++ Item 4/MapItem.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace Item_4
 {
-    internal abstract class MapItem
+    internal interface MapItem
     {
-        private int xPos;
-        private int yPos;
+        int XPos { get; set; }
+        int YPos { get; set; }
     }
 }
